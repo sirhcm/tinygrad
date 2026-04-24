32dd9232ce3d3c5e61499f56302da37bb749458b
Christopher Milan <chrismilan@ucla.edu>
2026-04-24 13:38:03 -0700

    fix nolocals pickled jit

--- tinygrad/engine/realize.py
+++ tinygrad/engine/realize.py
@@ -77,7 +77,7 @@ class CompiledRunner(Runner):
     if Device[self.p.device].renderer.has_local and local_size is None and all_int(self.p.global_size):
       local_size = optimize_local_size(self._prg, global_size, rawbufs)
       global_size = [g//l if g%l == 0 else g/l for g,l in zip(global_size, local_size)]
-      self.p = replace(self.p, global_size=global_size, local_size=local_size)
+      self.p.global_size, self.p.local_size = global_size, local_size
     return self._prg(*[x._buf for x in rawbufs], global_size=tuple(global_size), local_size=tuple(local_size) if local_size else None,
                      vals=tuple(var_vals[k.expr] if k.expr not in self.p.runtimevars else None for k in self.p.vars), wait=wait, timeout=timeout)
 
